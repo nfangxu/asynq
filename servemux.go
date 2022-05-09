5f4fb2af0b3ccc0afaca8d138225bb1b50d505b2
nfangxu <nfangxu@gmail.com>
2022-05-09 18:47:59 +0800

    feat: Support multi handler

--- servemux.go
+++ servemux.go
@@ -24,14 +24,18 @@ import (
 // with "images".
 type ServeMux struct {
 	mu  sync.RWMutex
-	m   map[string]muxEntry
-	es  []muxEntry // slice of entries sorted from longest to shortest.
+	m   map[string]*muxEntry
+	es  []*muxEntry // slice of entries sorted from longest to shortest.
 	mws []MiddlewareFunc
 }
 
 type muxEntry struct {
-	h       Handler
+	hs      []Handler
 	pattern string
+}
+
+func (e *muxEntry) Append(h Handler) {
+	e.hs = append(e.hs, h)
 }
 
 // MiddlewareFunc is a function which receives an asynq.Handler and returns another asynq.Handler.
@@ -47,8 +51,13 @@ func NewServeMux() *ServeMux {
 // ProcessTask dispatches the task to the handler whose
 // pattern most closely matches the task type.
 func (mux *ServeMux) ProcessTask(ctx context.Context, task *Task) error {
-	h, _ := mux.Handler(task)
-	return h.ProcessTask(ctx, task)
+	hs, _ := mux.Handler(task)
+	for _, h := range hs {
+		if err := h.ProcessTask(ctx, task); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // Handler returns the handler to use for the given task.
@@ -58,34 +67,37 @@ func (mux *ServeMux) ProcessTask(ctx context.Context, task *Task) error {
 //
 // If there is no registered handler that applies to the task,
 // handler returns a 'not found' handler which returns an error.
-func (mux *ServeMux) Handler(t *Task) (h Handler, pattern string) {
+func (mux *ServeMux) Handler(t *Task) (hs []Handler, pattern string) {
 	mux.mu.RLock()
 	defer mux.mu.RUnlock()
 
-	h, pattern = mux.match(t.Type())
-	if h == nil {
-		h, pattern = NotFoundHandler(), ""
+	hs, pattern = mux.match(t.Type())
+	if hs == nil {
+		hs, pattern = NotFoundHandler(), ""
 	}
-	for i := len(mux.mws) - 1; i >= 0; i-- {
-		h = mux.mws[i](h)
+	for j, h := range hs {
+		for i := len(mux.mws) - 1; i >= 0; i-- {
+			hs[j] = mux.mws[i](h)
+		}
 	}
-	return h, pattern
+
+	return hs, pattern
 }
 
 // Find a handler on a handler map given a typename string.
 // Most-specific (longest) pattern wins.
-func (mux *ServeMux) match(typename string) (h Handler, pattern string) {
+func (mux *ServeMux) match(typename string) (hs []Handler, pattern string) {
 	// Check for exact match first.
 	v, ok := mux.m[typename]
 	if ok {
-		return v.h, v.pattern
+		return v.hs, v.pattern
 	}
 
 	// Check for longest valid match.
 	// mux.es contains all patterns from longest to shortest.
 	for _, e := range mux.es {
 		if strings.HasPrefix(typename, e.pattern) {
-			return e.h, e.pattern
+			return e.hs, e.pattern
 		}
 	}
 	return nil, ""
@@ -104,19 +116,21 @@ func (mux *ServeMux) Handle(pattern string, handler Handler) {
 	if handler == nil {
 		panic("asynq: nil handler")
 	}
-	if _, exist := mux.m[pattern]; exist {
-		panic("asynq: multiple registrations for " + pattern)
-	}
 
 	if mux.m == nil {
-		mux.m = make(map[string]muxEntry)
+		mux.m = make(map[string]*muxEntry)
 	}
-	e := muxEntry{h: handler, pattern: pattern}
-	mux.m[pattern] = e
-	mux.es = appendSorted(mux.es, e)
+
+	if _, exist := mux.m[pattern]; !exist {
+		me := &muxEntry{pattern: pattern}
+		mux.m[pattern] = me
+		mux.es = appendSorted(mux.es, me)
+	}
+	e := mux.m[pattern]
+	e.Append(handler)
 }
 
-func appendSorted(es []muxEntry, e muxEntry) []muxEntry {
+func appendSorted(es []*muxEntry, e *muxEntry) []*muxEntry {
 	n := len(es)
 	i := sort.Search(n, func(i int) bool {
 		return len(es[i].pattern) < len(e.pattern)
@@ -125,8 +139,8 @@ func appendSorted(es []muxEntry, e muxEntry) []muxEntry {
 		return append(es, e)
 	}
 	// we now know that i points at where we want to insert.
-	es = append(es, muxEntry{}) // try to grow the slice in place, any entry works.
-	copy(es[i+1:], es[i:])      // shift shorter entries down.
+	es = append(es, &muxEntry{}) // try to grow the slice in place, any entry works.
+	copy(es[i+1:], es[i:])       // shift shorter entries down.
 	es[i] = e
 	return es
 }
@@ -155,4 +169,4 @@ func NotFound(ctx context.Context, task *Task) error {
 }
 
 // NotFoundHandler returns a simple task handler that returns a ``not found`` error.
-func NotFoundHandler() Handler { return HandlerFunc(NotFound) }
+func NotFoundHandler() []Handler { return []Handler{HandlerFunc(NotFound)} }
